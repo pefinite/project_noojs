2a9864fe37d50538fedc943064f306f5bdba931d
pefinite <argshub@outlook.com>
2024-06-17 16:08:35 +0600

    x

--- noo-1.0.1.js
+++ noo-1.0.1.js
@@ -27,6 +27,42 @@ var theMouseMoveCastingForNooJS = {x: 0, y: 0};
             }
         };
     };
+    noo.browser = {
+        footprint: function () {
+
+                let canvas = document.createElement('canvas');
+                let ctx = canvas.getContext('2d');
+                // https://www.browserleaks.com/canvas#how-does-it-work
+                let txt = 'CANVAS_FINGERPRINT';
+                ctx.textBaseline = "top";
+                ctx.font = "14px 'Arial'";
+                ctx.textBaseline = "alphabetic";
+                ctx.fillStyle = "#f60";
+                ctx.fillRect(125,1,62,20);
+                ctx.fillStyle = "#069";
+                ctx.fillText(txt, 2, 15);
+                ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+                ctx.fillText(txt, 4, 17);
+                return noo.string(canvas.toDataURL()).hash();
+
+        }
+    };
+    noo.string = function (string) {
+        return {
+            hash: function () {
+                let hs = 0;
+
+                if (string.length === 0) return hs;
+
+                for (i = 0; i < string.length; i++) {
+                    let char = string.charCodeAt(i);
+                    hs = ((hs << 5) - hs) + char;
+                    hs = hs & hs;
+                }
+                return hs;
+            }
+        };
+    };
     noo.alert = function (theMessage) {
         window.alert(theMessage);
     };
